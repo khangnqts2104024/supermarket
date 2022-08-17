b32714f496c42cad9b09d6683b7b860edcf88708
le anh trung <84488011+trungle2021@users.noreply.github.com>
2022-08-18 00:47:05 +0700

    Update Repository

---
+++ SuperMarket_Data/Services/Generic_Imp/Repository.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SuperMarket_DataAccess.Data;
+using SuperMarket_DataAccess.Repository.IRepository.GenericInterface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperMarket_DataAccess.Services.Generic_Imp
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _db;
+        internal DbSet<T>? dbSet;
+        public Repository(ApplicationDbContext db)
+        {
+            _db = db;
+            dbSet = _db.Set<T>();
+        }
+
+        public async Task<IEnumerable<T>> GetAll(Expression<Func<T, bool>>? filter = null, string? includeProperties = null)
+        {
+            IQueryable<T> query = dbSet;
+            if (filter != null)
+            {
+                query = query.Where(filter).AsNoTracking();
+            }
+            if (includeProperties != null)
+            {
+                foreach (var includeProp in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProp).AsNoTracking();
+                }
+            }
+            return await query.ToListAsync();
+        }
+
+        public async Task<T> GetFirstOrDefault(Expression<Func<T, bool>> filter, string? includeProperties = null)
+        {
+            IQueryable<T> query = dbSet;
+            query = query.Where(filter).AsNoTracking();
+            if (includeProperties != null)
+            {
+                foreach (var includeProp in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProp).AsNoTracking();
+                }
+            }
+            return await query.FirstOrDefaultAsync();
+        }
+
+
+        public async Task<T> Add(T entity)
+        {
+            await dbSet.AddAsync(entity);
+            return entity;
+        }
+
+        public async Task<IEnumerable<T>> AddRange(IEnumerable<T> entities)
+        {
+            await dbSet.AddRangeAsync(entities);
+            return entities;
+        }
+
+        public  void Remove(T entity)
+        {
+            dbSet.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            dbSet.RemoveRange(entities);
+        }
+    }
+}
---
+++ SuperMarket_Data/Services/UnitOfWork.cs
@@ -0,0 +1,31 @@
+﻿using SuperMarket_DataAccess.Data;
+using SuperMarket_DataAccess.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperMarket_DataAccess.Services
+{
+    public class UnitOfWork :IUnitOfWork
+    {
+        private readonly ApplicationDbContext _db;
+        public UnitOfWork(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+
+        public void ClearTracking()
+        {
+            _db.ChangeTracker.Clear();
+
+        }
+    }
+}
