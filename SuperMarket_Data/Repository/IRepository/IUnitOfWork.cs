7ec7639ce731977e9e19c3ad677711fb4b5ba12b
le anh trung <84488011+trungle2021@users.noreply.github.com>
2022-08-20 20:27:27 +0700

    ok

--- SuperMarket_Data/Repository/IRepository/IUnitOfWork.cs
+++ SuperMarket_Data/Repository/IRepository/IUnitOfWork.cs
@@ -13,22 +13,14 @@ namespace SuperMarket_DataAccess.Repository.IRepository
         IShoppingCart ShoppingCart { get; }
         IOrder Order { get; }
         IOrderDetail OrderDetail { get; }
-
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
-        void Save();
-=======
->>>>>>> Stashed changes
         ICategory Category { get; }
         IBranch Branch { get; }
         IBrand Brand { get; }
         IStock Stock { get; }
         ICoupon Coupon { get; }
         ICustomer Customer { get; }
-        Task<int> Save();
->>>>>>> 84651e2edcd6f53514780a71ae09b5877b145205
 
+        void Save();
         void ClearTracking();
     }
 }
