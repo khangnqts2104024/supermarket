205a1e66d60b3e667e7d9e2f5bb7f56d0103da29
le anh trung <84488011+trungle2021@users.noreply.github.com>
2022-08-20 07:35:23 +0700

    Register UnitOfWork (ShoppingCart, Order, OrderDetail)

---
+++ SuperMarket_Client/Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SuperMarket_DataAccess.Repository.IRepository;
+using SuperMarket_Models.Models;
+using System.Security.Claims;
+
+namespace SuperMarket_Client.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    [Authorize]
+    public class CartController : Controller
+    {
+        private readonly IUnitOfWork unitOfWork;
+
+        public CartController(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+            return View();
+        }
+
+       
+    }
+}
---
+++ SuperMarket_Models/ViewModels/ShoppingCartVM.cs
@@ -0,0 +1,14 @@
+﻿using SuperMarket_Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperMarket_Models.ViewModels
+{
+    public class ShoppingCartVM
+    {
+        
+    }
+}
