88e56275db7a1e90dc34eed21e3263d56d917c45
khoa954 <87458205+khoa954@users.noreply.github.com>
2022-08-22 12:09:08 +0700

    xong authorize

--- SuperMarket_Client/Areas/Admin/Controllers/HomeController.cs
+++ SuperMarket_Client/Areas/Admin/Controllers/HomeController.cs
@@ -3,10 +3,11 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace SuperMarket_Client.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     [Authorize]
     public class HomeController : Controller
     {
-        [Authorize(Roles = "Admin")]
+        [Authorize(Roles ="Admin")]
         public IActionResult Index()
         {
             return View();
