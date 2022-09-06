3a95970de4ab1bb0aa2d959b317e58ccf2691d34
trungle2021 <leanhtrung97@gmail.com>
2022-09-06 07:37:09 +0700

    fix layout image thumbnail details page

--- SuperMarket_Client/wwwroot/Asset/js/script.js
+++ SuperMarket_Client/wwwroot/Asset/js/script.js
@@ -2163,3 +2163,38 @@ var isApplied = false;
 
 
 })(window.jQuery);
+
+
+
+$(function () {
+
+    $('#thumbnail li').click(function () {
+        var thisIndex = $(this).index()
+
+        if (thisIndex < $('#thumbnail li.active').index()) {
+            prevImage(thisIndex, $(this).parents("#thumbnail").prev("#image-slider"));
+        } else if (thisIndex > $('#thumbnail li.active').index()) {
+            nextImage(thisIndex, $(this).parents("#thumbnail").prev("#image-slider"));
+        }
+
+        $('#thumbnail li.active').removeClass('active').css('border', '').css('opacity','');
+        $(this).addClass('active').css('border', '1px solid #86BC42').css('opacity', 1);
+    });
+});
+
+var width = $('#image-slider').width();
+
+function nextImage(newIndex, parent) {
+    parent.find('li').eq(newIndex).addClass('next-img').css('left', width).animate({ left: 0 }, 600);
+    parent.find('li.active-img').removeClass('active-img').css('left', '0').animate({ left: -width }, 600);
+    parent.find('li.next-img').attr('class', 'active-img');
+}
+function prevImage(newIndex, parent) {
+    parent.find('li').eq(newIndex).addClass('next-img').css('left', -width).animate({ left: 0 }, 600);
+    parent.find('li.active-img').removeClass('active-img').css('left', '0').animate({ left: width }, 600);
+    parent.find('li.next-img').attr('class', 'active-img');
+}
+
+/* Thumbails */
+//var ThumbailsWidth = ($('#image-slider').width() - 18.5) / 7;
+//$('#thumbnail li').find('img').css('width', ThumbailsWidth);
